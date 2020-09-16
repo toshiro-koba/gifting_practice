7f8b8f432106567dca254cd6db9afc6109110f60
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-17 08:10:46 +0900

    各ユーザーのギフティングページを用意

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update] do
+    resources :gifts, only: [:index, :new, :create]
+  end
 end
