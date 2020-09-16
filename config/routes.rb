c30507ef93939b8dfde992b78741409a356f55e3
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-16 22:55:01 +0900

    ビューを配置

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "gifts#index"
+  root to: "messages#index"
   resources :users, only: [:edit, :update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_121945) do
+ActiveRecord::Schema.define(version: 2020_09_16_134808) do
+
+  create_table "gifts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "giver_id"
+    t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["giver_id"], name: "index_gifts_on_giver_id"
+    t.index ["user_id"], name: "index_gifts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +35,6 @@ ActiveRecord::Schema.define(version: 2020_09_16_121945) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "gifts", "users"
+  add_foreign_key "gifts", "users", column: "giver_id"
 end
