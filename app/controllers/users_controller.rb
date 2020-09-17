f7ab5ce9fd72609c0dba16f034dee01811ea6476
toshiro-koba <kobato.231.0625@gmail.com>
2020-09-17 12:46:59 +0900

    ユーザー詳細ページにギフト履歴を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,25 @@ class UsersController < ApplicationController
   end
 
   def show
+    @users = User.all
     @user = User.find(params[:id])
+
+    @all_gifts_in_table = Gift.all
+    
+    @giftings = []
+    @all_gifts_in_table.each do |gift|
+      if gift.giver_id == current_user.id
+        @giftings << gift
+      end
+    end
+
+    @receivings = []
+    @all_gifts_in_table.each do |gift|
+      if gift.user_id == current_user.id
+        @receivings << gift
+      end
+    end
+    # binding.pry
   end
 
   private
@@ -20,3 +38,31 @@ class UsersController < ApplicationController
     params.require(:user).permit(:nickname, :email)
   end
 end
+
+
+# def index
+#   @rooms = Room.all.order('created_at DESC')
+#   @another_questions = []
+#   if user_signed_in?
+#     @entries = Entry.all.order('created_at DESC')
+#     @questions = []
+#     @question_ids = []
+#     @rooms.each do |room|
+#       @entries.each do |entry|
+#         next unless entry.room_id == room.id
+#         if entry.user_id == current_user.id
+#           @questions << room
+#           @question_ids << room.id
+#         end
+#       end
+#     end
+#     @another_question_ids = @rooms.ids - @question_ids
+#     @rooms.each do |room|
+#       @another_question_ids.each do |another_question_id|
+#         @another_questions << room if room.id == another_question_id
+#       end
+#     end
+#   else
+#     @another_questions = @rooms
+#   end
+# end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :gifts
   has_many :giftings, through: :gifts, source: :giver
   has_many :reverse_of_gifts, class_name: 'Gift', foreign_key: 'giver_id'
-  has_many :recivings, through: :reverse_of_gifts, source: :user
+  has_many :receivings, through: :reverse_of_gifts, source: :user
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
